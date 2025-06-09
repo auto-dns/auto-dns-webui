9742a9d4b84e60403989db8b5f4077642e25bdfd
Steven Carroll <stevencarroll90@gmail.com>
2025-06-09 21:25:31 +0000

    Ignoring dist directory. FIxing front end static build. Adding new cobra flags. Handling exit / kill signal.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ const __dirname = dirname(__filename)
 
 export default defineConfig({
   build: {
-    outDir: resolve(__dirname, '../backend/internal/frontend/dist'),
+    outDir: 'dist',
     emptyOutDir: true
   },
   server: {
