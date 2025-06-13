e92fad1ecd53e2fb6050deb4916e3486db561179
Steven Carroll <stevencarroll90@gmail.com>
2025-06-13 23:16:53 +0000

    Fixing search

--- frontend/src/utils/record.ts
+++ frontend/src/utils/record.ts
@@ -3,8 +3,25 @@ import { RecordEntry } from '../types';
 export const getRecordKey = (record: RecordEntry) => `${record.dnsRecord.name}|${record.dnsRecord.type}|${record.dnsRecord.value}|${record.metadata.containerId}`;
 
 export function enrichSearchable(records: RecordEntry[]) {
-  return records.map((r) => ({
-    ...r,
-    searchable: Object.values(r).join(' ').toLowerCase(),
-  }));
+  return records.map((r) => {
+    const { dnsRecord, metadata } = r;
+    const searchText = [
+      dnsRecord.name,
+      dnsRecord.type,
+      dnsRecord.value,
+      metadata.containerId,
+      metadata.containerName,
+      metadata.hostname,
+      metadata.created,
+      metadata.force?.toString(),
+    ]
+      .filter(Boolean)
+      .join(' ')
+      .toLowerCase();
+
+    return {
+      ...r,
+      searchable: searchText,
+    };
+  });
 }
